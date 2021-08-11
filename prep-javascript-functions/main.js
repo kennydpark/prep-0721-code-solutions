139e982dae147d41b0596c3843a7869e35b0efa0
Kenny Park <kennyparc@gmail.com>
2021-08-10 21:04:33 -0700

    Completed prep-javascript-functions exercise.

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,61 @@
+function addTwoNumbers(x, y) {
+  return x + y;
+}
+
+var addTwoNumbersResult = addTwoNumbers(4, 4);
+console.log('addTwoNumbers Exercise:', addTwoNumbersResult);
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+
+var convertHoursToMinutesResult = convertHoursToMinutes(2);
+console.log('convertHoursToMinutes Exercise:', convertHoursToMinutesResult);
+
+function getGreeting(name) {
+  return 'Hello ' + name + '!';
+}
+
+var getGreetingResult = getGreeting('World');
+console.log('getGreeting Exercise:', getGreetingResult);
+
+function addAndMultiplyBy5(num1, num2) {
+  return (num1 + num2) * 5;
+}
+
+var addAndMultiplyBy5Result = addAndMultiplyBy5(10, 5);
+console.log('addAndMultiplyBy Exercise:', addAndMultiplyBy5Result);
+
+function multiplyAndDivideBy5(num1, num2) {
+  return (num1 * num2) / 5;
+}
+
+var multiplyAndDivideBy5Result = multiplyAndDivideBy5(35, 10);
+console.log('multiplyAndDivideBy5 Exercise:', multiplyAndDivideBy5Result);
+
+function subtractTwoNumbers(num1, num2) {
+  return num1 - num2;
+}
+
+var subtractTwoNumbersResult = subtractTwoNumbers(22, 7);
+console.log('subtractTwoNumbers Exercise:', subtractTwoNumbersResult);
+
+function getCircleCircumference(radius) {
+  return (radius * 2) * Math.PI; // didn't know how else to input the value of pi correctly, so I just googled it)
+}
+
+var getCircleCircumferenceResult = getCircleCircumference(5);
+console.log('getCircleCircumference Exercise:', getCircleCircumferenceResult);
+
+function getFullName(firstName, lastName) {
+  return firstName + ' ' + lastName;
+}
+
+var getFullNameResult = getFullName('Kenny', 'Park');
+console.log('getFullname Exercise:', getFullNameResult);
+
+function cube(number) {
+  return number * number * number;
+}
+
+console.log('cube Exercise:', cube(5));

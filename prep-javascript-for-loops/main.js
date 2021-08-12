345c0e73b6402f674fd4565864ea193e86b03933
Kenny Park <kennyparc@gmail.com>
2021-08-11 21:30:04 -0700

    Completed prep-javascript-for-loops exercise.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,20 @@
+// create your loops here.
+// var num = '';
+
+// for (var i = 0; i < 10; i++) {
+//   console.log(num = i);
+// }
+
+// var num = -2;
+
+// for (var i = 0; i < 10; i++) {
+//   num += 2;
+//   console.log(num);
+// }
+
+// var num = 100;
+
+// for (var i = 0; i < 100; i++) {
+//   num--;
+//   console.log('Time till explosion:', num + '!');
+// }

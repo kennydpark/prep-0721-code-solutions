ed7e45e1644caadc73d2689ded8346a6facbfae9
Kenny Park <kennyparc@gmail.com>
2021-08-11 21:40:10 -0700

    Completed prep-javascript-for-loops exercise 3.

--- prep-javascript-for-loops/main.js
+++ prep-javascript-for-loops/main.js
@@ -6,16 +6,16 @@
 //   console.log(num);
 // }
 
-var num = -2;
+// var num = -2;
 
-for (var i = 0; i < 10; i++) {
-  num += 2;
-  console.log(num);
-}
-
-// var num = 100;
-
-// for (var i = 0; i < 100; i++) {
-//   num--;
-//   console.log('Time till explosion:', num + '!');
+// for (var i = 0; i < 10; i++) {
+//   num += 2;
+//   console.log(num);
 // }
+
+var num = 100;
+
+for (var i = 0; i < 100; i++) {
+  num--;
+  console.log('Time till explosion:', num + '!');
+}
